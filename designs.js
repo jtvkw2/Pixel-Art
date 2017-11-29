e26143240e610b331a3493ca7d0d7b564dd3b390
Jacob Voyles <jtvkw2@gmail.com>
2017-11-29 13:16:41 -0800

    changed grid method creation and added color changing

--- designs.js
+++ designs.js
@@ -3,27 +3,57 @@
 
 // When size is submitted by the user, call makeGrid()
 
-var lastClicked;
-var grid = clickableGrid(10,10,function(el,row,col){
+//var lastClicked;
+//var grid = makeGrid(document.getElementById("input_height").value,document.getElementById("input_width").value,function(el,row,col)
+//});
 
-    el.className='clicked';
-});
 
-document.body.appendChild(grid);
 
-function clickableGrid( rows, cols, callback ){
-    var grid = document.createElement('table');
-    grid.className = 'grid';
-    for (var r=0;r<rows;++r){
-        var tr = grid.appendChild(document.createElement('tr'));
-        for (var c=0;c<cols;++c){
-            var cell = tr.appendChild(document.createElement('td'));
-            cell.addEventListener('click',(function(el,r,c){
-                return function(){
-                    callback(el,r,c);
-                }
-            })(cell,r,c),false);
-        }
-    }
-    return grid;
+/**function changeColor(){
+  var color = document.getElementById("colorPicker").style.property;
+  console.log(color);
+
 }
+**/
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='grid'></div>");
+        };
+    };
+    $(".grid").height(960/x);
+    $(".grid").width(960/x);
+
+};
+
+// function that clears the grid
+function clearGrid(){
+    $(".grid").remove();
+};
+
+// function that prompts the user to select the number of boxes in a new grid
+// the function then also creates that new grid
+function refreshGrid(){
+    var z = prompt("How many boxes per side?");
+    clearGrid();
+    createGrid(z);
+};
+
+// create a 16x16 grid when the page loads
+// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
+// allows the click of a button to prompt the user to create a new grid
+$(document).ready(function() {
+    createGrid(10);
+
+    $(".grid").click(function() {
+        $(this).css("background-color", document.getElementById("colorPicker").value);
+        });
+
+    $(".newGrid").click(function() {
+        refreshGrid();
+
+        $(".grid").click(function() {
+        $(this).css("background-color", document.getElementById("colorPicker").value);
+        });
+    });
+});
