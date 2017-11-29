1fb6393aefa34eab69aead639e958ed89dc8a1bf
Jacob Voyles <jtvkw2@gmail.com>
2017-11-29 15:11:43 -0800

    grid system now responds to user input

--- designs.js
+++ designs.js
@@ -2,47 +2,42 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
-function createGrid(x, y) {
-    for (var rows = 0; rows < x; rows++) {
-        for (var columns = 0; columns < y; columns++) {
-            $("#container").append("<div class='grid'></div>");
-        };
+//function that creates grid
+function makeGrid(x, y) {
+  for (var rows = 0; rows < x; rows++) {
+    for (var columns = 0; columns < y; columns++) {
+      $("#container").append("<div class='grid'></div>");
     };
-    $(".grid").height(960/x);
-    $(".grid").width(960/y);
-
+  };
+  $(".grid").height(960/x);
+  $(".grid").width(960/y);
 };
 
 // function that clears the grid
 function clearGrid(){
-    $(".grid").remove();
+  $(".grid").remove();
 };
 
-// function that prompts the user to select the number of boxes in a new grid
-// the function then also creates that new grid
+//function to call to create a new grid
 function refreshGrid(){
-    var x = document.getElementById("input_height").value;
-    var y = document.getElementById("input_width").value;
-    clearGrid();
-    createGrid(x, y);
+  clearGrid();
+  makeGrid(document.getElementById("input_height").value, document.getElementById("input_width").value);
 };
 
-// create a 16x16 grid when the page loads
-// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
-// allows the click of a button to prompt the user to create a new grid
-$(document).ready(function() {
-    createGrid(10, 10);
+//creates a 10x10 grid onload
+//then contains funtions for when grid is clicked and starting a new grid
+$('#sizePicker').submit(function(event) {
+  $('#input').next().text();
+  event.preventDefault();
+  refreshGrid();
+  $(".grid").click(function() {
+    $(this).css("background-color", document.getElementById("colorPicker").value);
+  });
 
+  $(".newGrid").click(function() {
+    refreshGrid();
     $(".grid").click(function() {
-        $(this).css("background-color", document.getElementById("colorPicker").value);
-        });
-
-    $(".newGrid").click(function() {
-        refreshGrid();
-
-        $(".grid").click(function() {
-        $(this).css("background-color", document.getElementById("colorPicker").value);
-        });
+      $(this).css("background-color", document.getElementById("colorPicker").value);
     });
+  });
 });
