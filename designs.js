64c61f35e8c57f017f710cd49e45adbc13abfffa
Jacob Voyles <jtvkw2@gmail.com>
2017-11-29 13:40:10 -0800

    made grid reset button

--- designs.js
+++ designs.js
@@ -3,26 +3,14 @@
 
 // When size is submitted by the user, call makeGrid()
 
-//var lastClicked;
-//var grid = makeGrid(document.getElementById("input_height").value,document.getElementById("input_width").value,function(el,row,col)
-//});
-
-
-
-/**function changeColor(){
-  var color = document.getElementById("colorPicker").style.property;
-  console.log(color);
-
-}
-**/
-function createGrid(x) {
+function createGrid(x, y) {
     for (var rows = 0; rows < x; rows++) {
-        for (var columns = 0; columns < x; columns++) {
+        for (var columns = 0; columns < y; columns++) {
             $("#container").append("<div class='grid'></div>");
         };
     };
     $(".grid").height(960/x);
-    $(".grid").width(960/x);
+    $(".grid").width(960/y);
 
 };
 
@@ -34,16 +22,17 @@ function clearGrid(){
 // function that prompts the user to select the number of boxes in a new grid
 // the function then also creates that new grid
 function refreshGrid(){
-    var z = prompt("How many boxes per side?");
+    var x = document.getElementById("input_height").value;
+    var y = document.getElementById("input_width").value;
     clearGrid();
-    createGrid(z);
+    createGrid(x, y);
 };
 
 // create a 16x16 grid when the page loads
 // creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
 // allows the click of a button to prompt the user to create a new grid
 $(document).ready(function() {
-    createGrid(10);
+    createGrid(10, 10);
 
     $(".grid").click(function() {
         $(this).css("background-color", document.getElementById("colorPicker").value);
