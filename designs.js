52ad4cb12065d4dbf36c7e24c201120bcedd19b6
Jacob Voyles <jtvkw2@gmail.com>
2017-11-29 10:42:17 -0800

    added the grid and made it clickable

--- designs.js
+++ designs.js
@@ -3,8 +3,27 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+var lastClicked;
+var grid = clickableGrid(10,10,function(el,row,col){
 
-// Your code goes here!
+    el.className='clicked';
+});
 
+document.body.appendChild(grid);
+
+function clickableGrid( rows, cols, callback ){
+    var grid = document.createElement('table');
+    grid.className = 'grid';
+    for (var r=0;r<rows;++r){
+        var tr = grid.appendChild(document.createElement('tr'));
+        for (var c=0;c<cols;++c){
+            var cell = tr.appendChild(document.createElement('td'));
+            cell.addEventListener('click',(function(el,r,c){
+                return function(){
+                    callback(el,r,c);
+                }
+            })(cell,r,c),false);
+        }
+    }
+    return grid;
 }
