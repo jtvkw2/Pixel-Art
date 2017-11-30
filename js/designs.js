5063885a9471b199d4dec9c46bbc1583b86bdafd
Jacob Voyles <jtvkw2@gmail.com>
2017-11-30 09:51:03 -0800

    Removed extraneous functions and conformed to the style guide

--- js/designs.js
+++ js/designs.js
@@ -1,7 +1,3 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
 //Function that creates grid
 function makeGrid(x, y) {
   for (var rows = 0; rows < x; rows++) {
@@ -19,19 +15,12 @@ function refreshGrid(){
   makeGrid(document.getElementById("input_height").value, document.getElementById("input_width").value);
 };
 
-//Creates grid on submit
-//Also contains functions for changing color
+//Creates grid upon submit
+//Also contains function for changing color
 $('#sizePicker').submit(function(event) {
   event.preventDefault();
   refreshGrid();
   $(".layout").click(function() {
     $(this).css("background-color", document.getElementById("colorPicker").value);
-  });
-
-  $(".newGrid").click(function() {
-    refreshGrid();
-    $(".layout").click(function() {
-      $(this).css("background-color", document.getElementById("colorPicker").value);
-    });
   });
 });
